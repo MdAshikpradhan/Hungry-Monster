f7ab32747e3a5e4b94cf6d5f4622ff1f52c4f2f0
mdAshikPradhan <mdashikpradhan.com>
2021-02-09 14:42:41 +0600

    Added

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
-function myFunction() {
-    const search = document.getElementById('searchBar').value;
-    document.getElementById('food').innerHTML = search;
+
+const searchMeals = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals));
 }
 
 
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(res => res.json())
-.then(data => displayMeal(data))
-
-const displayMeal = food => {
-    const foodsDiv = document.getElementById('food');
+const displayMeals = meals => {
+    const mealsContainer = document.getElementById('mealContainer');
+    mealsContainer.innerText = '';
+    meals.forEach(meal => {
         const mealDiv = document.createElement('div');
-        mealDiv.className = 'meal';
-        const mealInfo = `
-        <h1>${food.idMeal}</h1>
+        mealDiv.className = 'mealDiv';
+        mealDiv.innerHTML = `
+            <div  onclick="displayMealsDetails('${meal.idMeal}')" data-id="${meal.idMeal}" >
+            <img src="${meal.strMealThumb}" alt="">
+            <h3 class="mt-2">${meal.strMeal}</h3>
+            </div>
         `;
-        mealDiv.innerHTML = mealInfo;
-        foodsDiv.appendChild(mealDiv);
+        mealsContainer.appendChild(mealDiv);
+    });
+}
+
+
+
+const displayMealsDetails = name => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`
+            fetch(url)
+            .then(res => res.json())
+            .then(data => ingredients(data.meals[0]))
+}
+
+const ingredients = ingredients => {
+    console.log(ingredients);
+    const mealIngredientsDiv = document.getElementById('mealIngredients');
+    mealIngredientsDiv.innerHTML = `
+        <div>
+        <img src="${ingredients.strMealThumb}" alter="">
+        </div>
+        <div>
+        <h3>${ingredients.strMeal}</h3>
+        <p>ingredients</p>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient1}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient2}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient3}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient4}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient5}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient6}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient7}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient8}</li>
+        <li> <i class="fas fa-check-square"></i> ${ingredients.strIngredient9}</li>
+        </div>
+    `
 }
